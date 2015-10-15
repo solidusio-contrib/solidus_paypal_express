90b58a53b8a1e650144432af848dd8829cbbc9d1
Dylan Kendal <dkendal@freerunningtechnologies.com>
2015-10-15 12:55:05 -0700

    Allow capture and credit from the admin interface

--- app/models/spree/paypal_express_checkout.rb
+++ app/models/spree/paypal_express_checkout.rb
@@ -1,4 +1,15 @@
 module Spree
   class PaypalExpressCheckout < ActiveRecord::Base
+    def actions
+      ["capture", "credit"]
+    end
+
+    def can_capture? payment
+      payment.pending?
+    end
+
+    def can_credit? payment
+      payment.completed?
+    end
   end
 end
